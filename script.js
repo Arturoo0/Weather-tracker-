8fdc47c11846e454e0baecefe618cd4132fb4393
Bruce Berrios <bberr022@fiu.edu>
2020-03-20 00:40:55 -0400

    Refactor column creation

--- script.js
+++ script.js
@@ -1,49 +1,42 @@
 
-addTracker = document.querySelector(".button");
+const addTracker = document.querySelector(".button");
 console.log(addTracker);
+
+const cardHTML = `
+<div class="card">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+`
+
+function createColumn() {
+  let newCol = document.createElement('div');
+  newCol.className = "col-lg-4 col-md-6";
+  newCol.innerHTML = cardHTML;
+
+  return newCol;
+}
 
 addTracker.onclick = () => {
   cardCols = document.querySelectorAll(".col-lg-4");
   cardContainer = document.querySelector("#card_container");
 
-  if (cardCols.length % 2 === 0){
-
+  if (cardCols.length % 3 === 0){
     let newRow = document.createElement('div');
     newRow.className = "row justify-content-center align-items-center";
 
-    let newCol = document.createElement('div');
-    newCol.className = "col-lg-4 col-md-6";
-
-    newCol.innerHTML = `
-    <div class="card">
-      <img src="..." class="card-img-top" alt="...">
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
-      </div>
-    </div>
-    `;
+    let newCol = createColumn();
 
     newRow.appendChild(newCol);
     cardContainer.appendChild(newRow);
 
   }else{
-    let rows = document.querySelectorAll(".row");
+    let lastRow = cardContainer.lastElementChild;
 
-    let newCol = document.createElement('div');
-    newCol.className = "col-lg-4 col-md-6";
-
-    newCol.innerHTML = `
-    <div class="card">
-      <img src="..." class="card-img-top" alt="...">
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
-      </div>
-    </div>
-    `;
-    rows[rows.length - 1].appendChild(newCol);
+    let newCol = createColumn();
+    lastRow.appendChild(newCol);
   }
 };
