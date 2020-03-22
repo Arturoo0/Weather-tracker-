b8234a72ce737088abbfd385c1a73a5b7d8e7003
Arturoo0 <arturoportelles@gmail.com>
2020-03-21 20:03:20 -0400

    add and remove animation

--- script.js
+++ script.js
@@ -1,38 +1,65 @@
 
 const addTracker = document.querySelector("#addTracker");
-const removeTracker = document.querySelector("#removeTracker");
+const removeTracker = document.querySelector(".removeTracker");
 
 const cardHTML = `
 <div class="card">
-  <div class="card-body animated fadeIn">
+  <div class="card-body">
     <h5 class="card-title">Card title</h5>
     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
     <div class="d-flex justify-content-between">
       <a href="#" class="btn btn-primary card-btn">Go somewhere</a>
-      <button class="removeTracker" type="button" name="button">🗑</button>
+      <button class="removeTracker" type="button" name="button" onclick="deleteTracker(this)"">🗑</button>
     </div>
   </div>
 </div>
 `
-
 function createColumn() {
   const newCol = document.createElement('div');
-  newCol.className = "col-lg-4 col-md-6";
+  newCol.className = "col-lg-4 col-md-6 animated slideInUp";
   newCol.innerHTML = cardHTML;
 
   return newCol;
 }
 
 // removeTracker.onclick = () => {
-//   const cardContainer = document.querySelector('#card_container');
-//   const lastRow = cardContainer.lastElementChild;
+//   // const cardContainer = document.querySelector('#card_container');
+//   // const lastRow = cardContainer.lastElementChild;
+//   //
+//   // lastRow.lastElementChild.remove();
+//   //
+//   // if (lastRow.innerHTML === ""){
+//   //   lastRow.remove();
+//   // }
+//   console.log('test');
 //
-//   lastRow.lastElementChild.remove();
-//
-//   if (lastRow.innerHTML === ""){
-//     lastRow.remove();
-//   }
 // }
+
+function deleteTracker(element){
+  element.parentNode.parentNode.parentNode.parentNode.remove();
+
+  let card_container = document.querySelector("#card_container");
+  const totalRows = document.querySelectorAll(".row");
+  let cardStack = document.querySelectorAll(".col-lg-4");
+
+  console.log(totalRows);
+  for (let i = 0; i < totalRows.length; i++){
+    totalRows[i].innerHTML = "";
+  }
+
+  let myArray = Array.from(cardStack)
+
+  for (let i = 0; i < totalRows.length; i++){
+    for (let j = 0; j < 2; j++){
+      if (myArray.length != 0){
+        totalRows[i].appendChild(myArray.shift());
+      }
+      else
+        break;
+    }
+  }
+
+}
 
 addTracker.onclick = () => {
   const cardCols = document.querySelectorAll(".col-lg-4");
