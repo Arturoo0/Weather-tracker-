8c67bb4fdb25774ccf99ab175147fe68b8c9dd11
Arturoo0 <arturoportelles@gmail.com>
2020-03-22 21:07:57 -0400

    added weather symbols to all cards

--- script.js
+++ script.js
@@ -8,25 +8,22 @@ const cardHTML = `
   <div class="card-body">
     <h5 class="card-title">Card title</h5>
     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-
     <div class="row">
       <div class="col-sm-6">
         <div class="card">
           <div class="card-body">
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <p class="card-text currentWeather">🌧</p>
           </div>
         </div>
       </div>
       <div class="col-sm-6">
         <div class="card">
           <div class="card-body">
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
           </div>
         </div>
       </div>
     </div>
-
-
     <div class="d-flex justify-content-between">
       <a href="#" class="btn btn-primary card-btn">Go somewhere</a>
       <button class="remove-btn" type="button" name="button" onclick="removeCard(event)">🗑</button>
