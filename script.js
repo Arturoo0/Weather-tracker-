0171d9092ac2f2cac7c087e37aa1a7c880c3e2c2
Arturoo0 <arturoportelles@gmail.com>
2020-03-22 20:37:33 -0400

    removed src images

--- script.js
+++ script.js
@@ -12,7 +12,6 @@ const cardHTML = `
     <div class="row">
       <div class="col-sm-6">
         <div class="card">
-          <img src="..." class="card-img-top" alt="...">
           <div class="card-body">
           <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
           </div>
@@ -20,7 +19,6 @@ const cardHTML = `
       </div>
       <div class="col-sm-6">
         <div class="card">
-          <img src="..." class="card-img-top" alt="...">
           <div class="card-body">
           <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
           </div>
