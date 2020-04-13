3cfab054629a487d257a51d25bed15a8e54818e4
Bruce Berrios <bberr022@fiu.edu>
2020-04-13 17:28:03 -0400

    Update card contents

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -6,27 +6,25 @@ const cardRow = cardContainer.querySelector("#card-row");
 const cardHTML = `
 <div class="card">
   <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <h2 class="card-title">...</h2>
     <div class="row">
       <div class="col-sm-6">
         <div class="card">
           <div class="card-body">
-          <p class="card-text current-weather">🌧</p>
+          <p class="card-text current-weather">...</p>
           </div>
         </div>
       </div>
       <div class="col-sm-6">
         <div class="card">
           <div class="card-body">
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-primary card-btn" data-toggle="modal" data-target="#forecast-modal" onclick="displayForecast(event)">View forecast</a>
+            <h3 class="card-title temp-text">...</h3>
           </div>
         </div>
       </div>
     </div>
     <div class="d-flex justify-content-between">
-      <a href="#" class="btn btn-primary card-btn">Go somewhere</a>
+      <a href="#" class="btn btn-primary card-btn" data-toggle="modal" data-target="#forecast-modal" onclick="displayForecast(event)">View forecast</a>
       <button class="remove-btn" type="button" name="button" onclick="removeCard(event)">🗑</button>
     </div>
   </div>
@@ -85,7 +83,7 @@ async function updateCard(card, location) {
   const locationData = await getLocationData(location);
 
   let cardTitle = card.querySelector(".card-title");
-  let cardTemp = card.querySelector(".card-text");
+  let cardTemp = card.querySelector(".temp-text");
   let cardWeatherSymbol = card.querySelector(".current-weather");
 
   cardTitle.innerHTML = `${locationData.name}`;
