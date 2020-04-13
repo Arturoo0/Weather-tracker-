683163063bba8b967cfe6672e3c2e84871769576
Arturoo0 <arturoportelles@gmail.com>
2020-04-13 16:52:22 -0400

    catch up

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -20,7 +20,7 @@ const cardHTML = `
         <div class="card">
           <div class="card-body">
             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-primary card-btn" data-toggle="modal" data-target="#forecast-modal" onclick="displayForecast(event)">View forecast</a>
+            <a href="#" class="btn btn-primary card-btn" data-toggle="modal" data-target="#forecast-modal" onclick="displayForecast(this)">View forecast</a>
           </div>
         </div>
       </div>
